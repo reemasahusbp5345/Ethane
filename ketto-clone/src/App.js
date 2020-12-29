a015f9bb33ac92ee280e5baeb2bb257b518135d0
faiitguy <faizkhan3597@gmail.com>
2020-12-29 12:57:51 +0530

    Folder Structure created

---
+++ ketto-clone/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Components/css/App.css'
+import Form from './Components/Form'
+function App() {
+  return (
+  <div className = "text-center">
+  <Form/>
+  </div>
+  )
+}
+
+export default App
---
+++ ketto-clone/src/Components/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './css/Form.css'
+
+function Form() {
+    return(
+        <div>
+                <h1>Form Component in How it works Page</h1>
+        </div>
+    )
+}
+export default Form
