05e14dd5033050c8bde7d262c44485574771cf7c
Reema <reemasahusbp5345@gmail.com>
2020-12-29 15:22:26 +0530

    navbar

---
+++ ketto-clone/src/Pages/BrowseFundraisers.jsx
@@ -0,0 +1,13 @@
+ import React, { Component } from 'react';
+ 
+ class BrowseFundraisers extends Component {
+     render() {
+         return (
+             <div>
+                 
+             </div>
+         );
+     }
+ }
+ 
+ export default BrowseFundraisers;
