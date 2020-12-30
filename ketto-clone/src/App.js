73b8d2dc22f22cd7a8802cf96b587e246c1e3be5
faiitguy <faizkhan3597@gmail.com>
2020-12-30 20:58:43 +0530

    Demo Component

--- ketto-clone/src/App.js
+++ ketto-clone/src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './Components/css/App.css'
 import Form from './Components/Form'
 import Demo from './Components/Demo'
+import Carousel from './Components/Carousel'
+import Features from './Components/Features'
 function App() {
   return (
   <div className = "text-center">
-  <Form/>
+  {/* <Form/>
   <Demo/>
+  <Carousel/> */}
+  <Features/>
   </div>
   )
 }
---
+++ ketto-clone/src/Components/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/Carousel.css'
+
+function Carousel() {
+    return(
+        <div>
+            <div className = "carousel-container">
+            </div>    
+        </div>
+    )
+}
+export default Carousel
