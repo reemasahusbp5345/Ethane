6bdce91156931e67732c7f5f3b158948061f0896
Reema <reemasahusbp5345@gmail.com>
2021-01-01 11:24:42 +0530

    homepage merge

--- ketto-clone/src/Pages/Home.jsx
+++ ketto-clone/src/Pages/Home.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Banner from "../Components/Banner/Banner"
+import Causes from '../Components/Causes/Causes';
+import SuccessStories from '../Components/SuccessStories/SuccessStories';
+import TalkToUs from '../Components/TalkToUs/TalkToUs';
+import WhatWeOffer from '../Components/WhatWeOffer/WhatWeOffer';
 
 class Home extends Component {
     render() {
         return (
-            <div style={{marginTop:"300px"}}>
-                Home
+            <div>
+               <Banner/>
+               <Causes/>
+               <WhatWeOffer/>
+               <SuccessStories/>
+               <TalkToUs/>
             </div>
         );
     }
